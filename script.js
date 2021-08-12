d6a53373922a3ef309a8bfd3fcc51316ddd8970e
Rajarajan_rt <47898159+RAJARAJAN-RT@users.noreply.github.com>
2021-08-12 21:05:21 +0530

    NEW

---
+++ script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "RAJARAJAN T",
+      "email": "rajarat6@gamil.com",
+      "phone": 9578741769,
+      "degree": "B.E",
+      "location": {
+        "address": "15/16 Sainath Enclave ShenoyNagar",
+        "postalCode": 600030,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/rajarajan-t-685958219/",
+          "github":"https://github.com/RAJARAJAN-RT"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The Photophilehub PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Thiagarajar College Of Engineering",
+        "department": "Mechatronics",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
